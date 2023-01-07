1262d21e31b590359a68e8d410e4e3a8a9350a2c
intux69 <intux69@gmail.com>
2023-01-07 16:50:10 +0100

    intux

---
+++ app/src/test/java/com/intux/intuxapp/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.intux.intuxapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * intux local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class intuxUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
